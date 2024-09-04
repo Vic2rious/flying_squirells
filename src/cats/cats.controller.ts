63027756140f3748c177c9f24701fe6d3de5b8ba
Vic2rious <victor.ava.ivanov@gmail.com>
2024-09-04 15:13:30 +0300

    test1

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Param, Delete } from '@nestjs/common';
+//import { CreateCatDto } from './dto/create-cat.dto';
+
+@Controller('cats')
+export class CatsController {
+  // @Post()
+  // create(@Body() createCatDto: CreateCatDto) {
+  //   return 'This action adds a new cat';
+  // }
+
+  // @Get()
+  // findAll(@Query() query: ListAllEntities) {
+  //   return `This action returns all cats (limit: ${query.limit} items)`;
+  // }
+
+  @Get()
+  findAll() {
+    return `This action returns all cats`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} cat`;
+  }
+
+  // @Put(':id')
+  // update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+  //   return `This action updates a #${id} cat`;
+  // }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
+  }
+}
