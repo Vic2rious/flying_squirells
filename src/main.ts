d2eafc614853f28a30de65713d803dd5d8810f91
Vic2rious <victor.ava.ivanov@gmail.com>
2024-09-05 17:00:13 +0300

    test1

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   await app.listen(3000);
   console.log(`Server listening on: http://localhost:3000/`);
 
-  app.enableCors({
-    origin: '*', // Replace with your frontend's URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true, // If you need to include cookies or authentication info
-  });
+  // app.enableCors({
+  //   origin: true, // Replace with your frontend's URL
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   credentials: true, // If you need to include cookies or authentication info
+  // });
 }
 
 bootstrap();
