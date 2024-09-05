05d979b30805f9eacfb004e8a970677ba569dc5c
Vic2rious <victor.ava.ivanov@gmail.com>
2024-09-05 16:46:57 +0300

    allowed cors for frontend

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
   console.log(`Server listening on: http://localhost:3000/`);
+
+  app.enableCors({
+    origin: 'http://localhost:4200/', // Replace with your frontend's URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // If you need to include cookies or authentication info
+  });
 }
+
 bootstrap();
